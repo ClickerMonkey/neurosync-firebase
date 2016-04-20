1ce0e99eafec77ccd51971394ecb9a23764ea199
Philip Diffenderfer <pdiffenderfer@gmail.com>
2016-04-19 21:49:45 -0400

    Resolves #1

--- build/rekord-firebase.js
+++ build/rekord-firebase.js
@@ -1,6 +1,5 @@
 (function(Firebase, global, undefined)
 {
-
   var cache = {};
 
   var Rekord_live = Rekord.live;
@@ -11,187 +10,177 @@
     return url in cache ? cache[ url ] : cache[ url ] = new Firebase( url );
   };
 
-  if ( !Rekord.liveSet )
+  Rekord.setLive(function(database)
   {
-    Rekord.live = function(database)
+    if ( !database.api )
     {
-      if ( !database.api )
-      {
-        return Rekord_live.call( this, database );
-      }
+      return Rekord_live.call( this, database );
+    }
 
-      var fire = database.api;
+    var fire = database.api;
 
-      function handleSave(snapshot)
-      {
-        var data = snapshot.val();
-        var key = snapshot.key();
+    function handleSave(snapshot)
+    {
+      var data = snapshot.val();
+      var key = snapshot.key();
 
-        database.liveSave( key, data );
-      }
+      database.liveSave( key, data );
+    }
 
-      function handleRemove(snapshot)
-      {
-        var data = snapshot.val();
-        var key = snapshot.key();
+    function handleRemove(snapshot)
+    {
+      var data = snapshot.val();
+      var key = snapshot.key();
 
-        database.liveRemove( key );
-      }
+      database.liveRemove( key );
+    }
 
-      fire.on( 'child_added', handleSave );
-      fire.on( 'child_changed', handleSave );
-      fire.on( 'child_removed', handleRemove );
-    
-      return {
-        firebase: fire,
-        save: Rekord.noop,
-        remove: Rekord.noop
-      };
+    fire.on( 'child_added', handleSave );
+    fire.on( 'child_changed', handleSave );
+    fire.on( 'child_removed', handleRemove );
+
+    return {
+      firebase: fire,
+      save: Rekord.noop,
+      remove: Rekord.noop
     };
+  });
 
-    Rekord.liveSet = true;
-  }
-
-  if ( !Rekord.restSet )
+  Rekord.setRest(function(database)
   {
-    Rekord.rest = function(database)
+    if ( !database.api )
     {
-      if ( !database.api )
-      {
-        return Rekord_rest.call( this, database );
-      }
+      return Rekord_rest.call( this, database );
+    }
 
-      var fire = database.api;
+    var fire = database.api;
 
-      function createCallback(success, failure)
+    function createCallback(success, failure)
+    {
+      return function onOperation(error)
       {
-        return function onOperation(error) 
+        if ( error )
         {
-          if ( error ) 
-          {
-            failure( {}, error );
-          } 
-          else 
-          {
-            success( {} );
-          }
-        };
-      }
-
-      function clearUndefined(obj)
-      {
-        for (var prop in obj)
+          failure( {}, error );
+        }
+        else
         {
-          if ( obj[ prop ] === undefined )
-          {
-            delete obj[ prop ];
-          }
+          success( {} );
+        }
+      };
+    }
+
+    function clearUndefined(obj)
+    {
+      for (var prop in obj)
+      {
+        if ( obj[ prop ] === undefined )
+        {
+          delete obj[ prop ];
         }
       }
+    }
 
-      return {
+    return {
 
-        firebase: fire,
+      firebase: fire,
 
-        all: function( success, failure )
+      all: function( success, failure )
+      {
+        if ( Rekord.forceOffline )
         {
-          if ( Rekord.forceOffline )
-          {
-            return failure( [], 0 );
-          }
+          return failure( [], 0 );
+        }
 
-          function onAll(snapshot)
-          {
-            var data = snapshot.val();
-            var models = [];
+        function onAll(snapshot)
+        {
+          var data = snapshot.val();
+          var models = [];
 
-            for (var key in data)
+          for (var key in data)
+          {
+            var model = data[ key ];
+
+            if ( Rekord.isObject( model ) )
             {
-              var model = data[ key ];
-
-              if ( Rekord.isObject( model ) )
-              {
-                models.push( model );
-              }
+              models.push( model );
             }
-
-            success( models );
           }
 
-          function onAllError(error)
-          {
-            failure( [], error.code );
-          }
-
-          fire.once( 'value', onAll, onAllError );
-        },
-
-        get: function( model, success, failure )
-        {
-          if ( Rekord.forceOffline )
-          {
-            return failure( {}, 0 );
-          }
-
-          function onGet(snapshot)
-          {
-            var data = snapshot.val();
-
-            success( data );
-          }
-
-          function onGetError(error)
-          {
-            failure( {}, error.code );
-          }
-
-          file.child( model.$key() ).once( 'value', onGet, onGetError );
-        },
-
-        create: function( model, encoded, success, failure )
-        {
-          if ( Rekord.forceOffline )
-          {
-            return failure( {}, 0 );
-          }
-
-          clearUndefined( encoded );
-
-          fire.child( model.$key() ).set( encoded, createCallback( success, failure ) );
-        },
-
-        update: function( model, encoded, success, failure )
-        {
-          if ( Rekord.forceOffline )
-          {
-            return failure( {}, 0 );
-          }
-
-          clearUndefined( encoded );
-
-          fire.child( model.$key() ).update( encoded, createCallback( success, failure ) );
-        },
-
-        remove: function( model, success, failure )
-        {
-          if ( Rekord.forceOffline )
-          {
-            return failure( {}, 0 );
-          }
-
-          fire.child( model.$key() ).remove( createCallback( success, failure ) );
-        },
-
-        query: function( query, success, failure )
-        {
-          success( [] );
+          success( models );
         }
 
-      };
+        function onAllError(error)
+        {
+          failure( [], error.code );
+        }
+
+        fire.once( 'value', onAll, onAllError );
+      },
+
+      get: function( model, success, failure )
+      {
+        if ( Rekord.forceOffline )
+        {
+          return failure( {}, 0 );
+        }
+
+        function onGet(snapshot)
+        {
+          var data = snapshot.val();
+
+          success( data );
+        }
+
+        function onGetError(error)
+        {
+          failure( {}, error.code );
+        }
+
+        file.child( model.$key() ).once( 'value', onGet, onGetError );
+      },
+
+      create: function( model, encoded, success, failure )
+      {
+        if ( Rekord.forceOffline )
+        {
+          return failure( {}, 0 );
+        }
+
+        clearUndefined( encoded );
+
+        fire.child( model.$key() ).set( encoded, createCallback( success, failure ) );
+      },
+
+      update: function( model, encoded, success, failure )
+      {
+        if ( Rekord.forceOffline )
+        {
+          return failure( {}, 0 );
+        }
+
+        clearUndefined( encoded );
+
+        fire.child( model.$key() ).update( encoded, createCallback( success, failure ) );
+      },
+
+      remove: function( model, success, failure )
+      {
+        if ( Rekord.forceOffline )
+        {
+          return failure( {}, 0 );
+        }
+
+        fire.child( model.$key() ).remove( createCallback( success, failure ) );
+      },
+
+      query: function( url, query, success, failure )
+      {
+        success( [] );
+      }
 
     };
 
-    Rekord.restSet = true;
-  }
+  });
 
 })( Firebase, this );
